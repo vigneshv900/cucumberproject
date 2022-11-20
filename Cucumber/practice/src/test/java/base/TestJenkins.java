177bbdb2ed34106f58211d1e39d8aa968fb22749
vigneshv900 <118652786+vigneshv900@users.noreply.github.com>
2022-11-20 15:13:42 +0530

    Update TestJenkins.java

--- Cucumber/practice/src/test/java/base/TestJenkins.java
+++ Cucumber/practice/src/test/java/base/TestJenkins.java
@@ -1,11 +1,15 @@
 package base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class TestJenkins {
+import io.github.bonigarcia.wdm.WebDriverManager;
 
+public class TestJenkins {
+WebDriver driver;
 	@BeforeMethod
 	public void setUp()
  {
@@ -14,15 +18,18 @@ public class TestJenkins {
 	
 	
 	@Test
-	public void test()
+	public void test() throws InterruptedException
 	{
-		
-		System.out.println("TEst");
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		Thread.sleep(3000);
 		}
 	
 	
 	@AfterMethod
 	public void tearDown() {
 		System.out.println("After");
+		driver.quit();
 	}
 	}
